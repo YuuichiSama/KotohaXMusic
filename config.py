049e2815f3df94264a3ed00b3a4caea877329f31
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-05-08 18:54:45 +0530

    Update config.py
    Signed-off-by: YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/YuuichiSama/KotohaXMusic",
+    "https://t.me/yuuichi_sama",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
