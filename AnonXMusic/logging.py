4507a727e0218d18c5b6b2cce9986b62a8dd047c
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-07-01 17:57:22 +0530

    Update logging.py
    Signed-off-by: YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -1,19 +1,25 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
+# Set log level from environment variable or default to INFO
+log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
+
+# Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=log_level,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        RotatingFileHandler("log.txt", maxBytes=1000000, backupCount=5),
         logging.StreamHandler(),
     ],
 )
 
+# Set specific log levels for certain libraries
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
